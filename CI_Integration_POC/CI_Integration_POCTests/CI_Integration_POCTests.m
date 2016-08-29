8b1f00ceb42663360e4c47953c9f1177a0f44ba0
ambujshukla <ambuj.shukla99@gmail.com>
2016-08-29 15:58:37 +0530

    Commit files

--- CI_Integration_POC/CI_Integration_POCTests/CI_Integration_POCTests.m
+++ CI_Integration_POC/CI_Integration_POCTests/CI_Integration_POCTests.m
@@ -24,7 +24,7 @@
     [super tearDown];
 }
 
-- (void)testExample {
+- (void)testExample1 {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
